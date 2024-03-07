ac1bb20521d635cf96b8e91468510ac6d7bbbb0c
CamilaIsabel <109829462+CamilaIsabel@users.noreply.github.com>
2024-03-07 08:28:38 -0300

    CL - call toasters

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import LayoutProvider from './layouts/LayoutProvider.vue';
+import AppToasterWrapper from './components/shared/toaster/AppToasterWrapper.vue';
 </script>
 
 <template>
   <LayoutProvider />
+
+  <AppToasterWrapper />
 </template>
