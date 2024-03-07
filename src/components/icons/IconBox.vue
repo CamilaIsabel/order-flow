17fecab6fe487aaecc6d811f34eda79dff656228
CamilaIsabel <109829462+CamilaIsabel@users.noreply.github.com>
2024-03-07 01:56:59 -0300

    CL - add product component

---
+++ src/components/icons/IconBox.vue
@@ -0,0 +1,58 @@
+<template>
+  <svg
+    width="13"
+    height="13"
+    viewBox="0 0 13 13"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_5853_8893)">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M11.0729 3.54194L6.05484 1.01898L1.02567 3.51961L1.0145 8.5432L6.03251 11.0662L11.0617 8.56554L11.0729 3.54194Z"
+        stroke="currentColor"
+        stroke-width="0.753541"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M1.02734 3.51953L6.04628 6.04296"
+        stroke="currentColor"
+        stroke-width="0.753541"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M6.03571 11.0675L6.04688 6.04297"
+        stroke="currentColor"
+        stroke-width="0.753541"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11.0753 3.54242L6.0452 6.04351"
+        stroke="currentColor"
+        stroke-width="0.753541"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8.56749 2.2807L3.53738 4.78179"
+        stroke="currentColor"
+        stroke-width="0.753541"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_5853_8893">
+        <rect
+          width="12.0567"
+          height="12.0567"
+          fill="currentColor"
+          transform="translate(0.0273438) rotate(0.127364)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
---
+++ src/pages/purchase-orders/components/Product.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { toRefs } from 'vue';
+
+interface Product {
+  name: string;
+  sku: string;
+  quantity: number;
+  price: string;
+}
+
+type ProductProps = {
+  product: Product;
+};
+
+const props = defineProps<ProductProps>();
+const { product } = toRefs(props);
+</script>
+
+<template>
+  <div class="border w-80 flex flex-col gap-y-2 border-gray-600 p-5 rounded-xl">
+    <div class="flex items-center gap-x-2 text-gray-600">
+      <IconBox class="w-5 h-5" />
+      <span>Name:</span>
+      <span class="text-white">{{ product.name || '-' }}</span>
+    </div>
+
+    <div class="flex items-center gap-x-2 text-gray-600">
+      <IconSku class="w-5 h-5" />
+      <span>Sku:</span>
+      <span class="text-white">{{ product.sku || '-' }}</span>
+    </div>
+
+    <div class="flex items-center gap-x-2 text-gray-600">
+      <IconQuantity class="w-5 h-5" />
+      <span>Quantity:</span
+      ><span class="text-white">{{ product.quantity || '-' }}</span>
+    </div>
+
+    <div class="flex items-center gap-x-2 text-gray-600">
+      <IconTag class="w-5 h-5" />
+      <span>Price:</span>
+      <span class="text-white">{{ product.price || '-' }}</span>
+    </div>
+  </div>
+</template>
